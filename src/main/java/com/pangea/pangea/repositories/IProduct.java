f7d89a6d121060aee0c214d044141010c2d374fd
Jadamartinez02 <javier.martinez02@globant.com>
2026-02-02 15:54:42 -0500

    add condition to saveProduct

--- src/main/java/com/pangea/pangea/repositories/IProduct.java
+++ src/main/java/com/pangea/pangea/repositories/IProduct.java
@@ -1,9 +1,10 @@
 package com.pangea.pangea.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pangea.pangea.models.Product;
-
+@Repository
 public interface IProduct extends JpaRepository<Product, Integer> {
 
 }
